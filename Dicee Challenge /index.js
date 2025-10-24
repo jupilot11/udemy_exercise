e2d6ccd319cb7c33d13498bad848777f22d488f1
jufiel0711 <104879762+jufiel0711@users.noreply.github.com>
2025-10-24 12:23:57 +0800

    use id for each dice elemetn

--- Dicee Challenge /index.js
+++ Dicee Challenge /index.js
@@ -5,16 +5,17 @@ var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
 var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-var image1 = document.querySelectorAll("img")[0];
+var image1 = document.getElementById("image1");
 
 image1.setAttribute("src", randomImageSource);
-
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+var image2 = document.getElementById("image2");
+
+image2.setAttribute("src", randomImageSource2);
 
 
 //If player 1 wins
