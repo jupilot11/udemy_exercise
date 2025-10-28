42f1459eed1ce15c4594a37c722186ce506a6e41
jufiel0711 <104879762+jufiel0711@users.noreply.github.com>
2025-10-28 09:42:17 +0800

    drum kit exercise

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,54 @@
+var buttons = document.querySelectorAll(".drum ");
+
+for (var index = 0; index < buttons.length; index++) {
+  var button = document.querySelectorAll(".drum ")[index];
+  button.addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+      case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+      case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+      case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+      case "j":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+
+      case "k":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+      case "l":
+        var kickBass = new Audio("sounds/kick-bass.mp3");
+        kickBass.play();
+        break;
+
+      default:
+        console.log(buttonInnerHTML);
+    } 
+
+  });
+}
+
+function handleClick() {
+    // alert("Drum kit clicked! Button index: " + index);
+}
+
